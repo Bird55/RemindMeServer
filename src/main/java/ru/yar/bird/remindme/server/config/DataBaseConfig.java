c10dba6b79591ee2b22c33a395262656dfc54cb3
Alexey Solovyev <bird55@narod.ru>
2015-12-13 08:31:51 +0300

    Started configure datasource for work with database.

---
+++ src/main/java/ru/yar/bird/remindme/server/config/DataBaseConfig.java
@@ -0,0 +1,24 @@
+package ru.yar.bird.remindme.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableJpaRepositories("ru.yar.remindme.server.repository")
+@EnableTransactionManagement
+@PropertySource("classpath:db.properties")
+@ComponentScan("ru.yar.remindme.server")
+public class DataBaseConfig {
+
+    @Resource
+    private Environment env;
+
+}
+
+
